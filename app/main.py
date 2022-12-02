7f90a099ef6e081c386c7c9f7d523c5660ad618f
StanDoge <StanlyDoge@gmail.com>
2022-12-02 17:53:51 -0600

    main endpoints

--- app/main.py
+++ app/main.py
@@ -7,6 +7,22 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("")
+def get_departament():
+    ...
+
+@app.put("")
+def update_departament():
+    ...
+
+@app.get("")
+def get_township():
+    ...
+
+@app.put("")
+def update_township():
+    ...
+
 if __name__ == "__main__":
 
     uvicorn.run("main:app")
