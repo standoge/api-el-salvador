3e5196c390bd7c1aac31b91c51875c6083c66546
StanDoge <stanlydoge@gmail.com>
2022-12-04 18:39:38 -0600

    `fix`: module call in tests/

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,13 @@
 import unittest
+import sys
 
-from app import app
+sys.path.insert(0, "/home/doge/development/Python/api/app")
+
+from main import app
 
 
 class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
-
-    def test_index(self):
-        response = self.app.get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_departaments(self):
-        response = self.app.get("/?department=Sonsonate")
-        self.assertEqual(response.status_code, 200)
-
-    def test_zonessv(self):
-        response = self.app.get("/?zone=Oriental")
-        self.assertEqual(response.status_code, 200)
+    ...
 
 
 if __name__ == "__main__":
