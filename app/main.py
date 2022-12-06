71e3725e7afa2f0ec072559489e4375812c0c71e
StanDoge <stanlydoge@gmail.com>
2022-12-05 22:02:36 -0600

    `docs`: update requirements

--- app/main.py
+++ app/main.py
@@ -9,9 +9,7 @@ from db import SessionLocal
 
 
 def db_connection():
-    """
-    Generates db connection for aeach request
-    """
+    """Generates db connection for aeach request."""
     db = SessionLocal()
     try:
         yield db
@@ -30,9 +28,7 @@ def read_root():
 # dep is get it from search bar writing its name
 @app.get("/departaments/{name}", response_model=schemas.Departament)
 def read_departament(name: str, db: Session = Depends(db_connection)):
-    """
-    Returns departaments data in json format
-    """
+    """Returns departaments data in json format."""
 
     db_departament = crud.get_departament(db, name)
 
@@ -42,29 +38,25 @@ def read_departament(name: str, db: Session = Depends(db_connection)):
 
 
 # dep is updated from <form> UI maybe
-@app.put("/departaments")
-def update_departament(departament: str = Body()):
-    """
-    Update departaments information
-    """
-    ...
+# @app.put("/departaments")
+# def update_departament(departament: str = Body()):
+#     """Update departaments information."""
+#     ...
 
 
 # equal than dep
 @app.get("/townships")
 def read_township(township: str = Query(min_length=5, max_length=19)):
-    """
-    Returns towships data in json format
-    """
-    ...
+    """Returns towships data in json format."""
+    pass
 
 
 # equal than dep
-@app.put("/townships")
-def update_township(
-    township: str = Body(),
-):
-    ...
+# @app.put("/townships")
+# def update_township(
+#     township: str = Body(),
+# ):
+#     ...
 
 
 if __name__ == "__main__":
