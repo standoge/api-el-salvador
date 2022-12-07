1cfed9ad29ceffd8b8fd14ae864a983cb968a0ce
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-07 15:53:33 -0600

    feat: allow relationship back populates

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from db import Base
-
 
 class Departament(Base):
     __tablename__ = "depsv"
@@ -10,18 +10,22 @@ class Departament(Base):
     depname = Column(String)
     isocode = Column(String)
     zonesv_id = Column(Integer, ForeignKey("zonesv.id"))
-
+    muns = relationship("Township", lazy="joined", back_populates="departament")
+    zone = relationship("Zone", lazy="joined", back_populates="departaments")
 
 class Township(Base):
     __tablename__ = "munsv"
 
     id = Column(Integer, primary_key=True, index=True)
     munname = Column(String)
-    depsv_id = Column(Integer, ForeignKey("depsv.id"))
+    depsv_id = Column(Integer, ForeignKey("depsv.id"), nullable=False)
+    departament = relationship("Departament", lazy="joined", back_populates="muns")
+
 
 
 class Zone(Base):
     __tablename__ = "zonesv"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    zonename = Column(String)
+    departaments = relationship("Departament", lazy="joined", back_populates="zone")
