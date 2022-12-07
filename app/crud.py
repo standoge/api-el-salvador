677468586f2f9b7599605c990638801ad1d4c4b8
StanDoge <stanlydoge@gmail.com>
2022-12-06 21:55:59 -0600

    docstrings

--- app/crud.py
+++ app/crud.py
@@ -5,9 +5,12 @@ import models
 
 
 def error_message(request):
+    """Raise exception when path argument isn't in db"""
+
     def wrapper(*args):
+        """Wrap query db function to handle exception"""
         response = request(*args)
-        if response == None:
+        if response is None:
             raise HTTPException(status_code=404, detail="That values doesn't exists")
         return response
 
@@ -17,7 +20,6 @@ def error_message(request):
 @error_message
 def get_departament(db: Session, dp_name: str):
     """Returns the first match with dp_name argument in depsv table."""
-
     query_response = (
         db.query(models.Departament)
         .filter(models.Departament.depname == dp_name)
@@ -30,7 +32,6 @@ def get_departament(db: Session, dp_name: str):
 @error_message
 def get_township(db: Session, mun_name: str):
     """Returns the first match with tws_name argument in munsv table."""
-
     query_response = (
         db.query(models.Township).filter(models.Township.munname == mun_name).first()
     )
