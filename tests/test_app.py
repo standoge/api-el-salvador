a3f61a061cfbb4fc6a998ca32d5ed7e1317ec8c6
StanDoge <stanlydoge@gmail.com>
2022-12-08 20:52:36 -0600

    status test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,15 @@
 import unittest
+import requests
+
+LOCAL_HOST = "http://localhost:8000"
 
 
 class TestApp(unittest.TestCase):
     """Unit test for endpoints"""
+
+    def test_status(self):
+        response = requests.get(LOCAL_HOST)
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
