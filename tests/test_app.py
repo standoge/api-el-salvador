9686ac9ecab2a51589c94d081adfc0f34b741d96
StanDoge <stanlydoge@gmail.com>
2022-12-08 21:34:18 -0600

    `test:` unittest

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,14 +8,17 @@ class TestApp(unittest.TestCase):
     """Unit test for endpoints"""
 
     def test_status(self):
+        """Test to check if status code in root endpoint is OK."""
         response = requests.get(LOCAL_HOST)
         self.assertEqual(response.status_code, 200)
 
     def test_read_departament(self):
+        """Test to check if departaments endpoint is responding."""
         response = requests.get(LOCAL_HOST + "/departaments/San Salvador")
         self.assertEqual(response.json()["depname"], "San Salvador")
 
     def test_read_municipality(self):
+        """Test to check if municipalities endpoint is responding."""
         response = requests.get(LOCAL_HOST + "/townships/Colon")
         self.assertEqual(response.json()["munname"], "Colón")
 
