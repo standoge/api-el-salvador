4cafdc5c7ec445fec2452554fef7f93cbdf3db4b
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-11 20:16:22 -0600

    `feat`: was allowed Cross-Origin Resource Sharing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import departaments, municipalities, zones
-
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["get"],
+)
 
 # routes
 app.include_router(departaments.router)
