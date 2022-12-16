9b4290f72497fe3c3e00399cb6c5713ad9348950
StanDoge <stanlydoge@gmail.com>
2022-12-15 18:29:36 -0600

    `fix:` pydantic data types
    Versions < Python 3.10 doesn't supports | between type definitions

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.routers import departaments, municipalities, zones
 
 app = FastAPI()
 
-origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
+origins = ["http://localhost:3000", "http://127.0.0.1:3000", "http://0.0.0.0"]
 
 app.add_middleware(
     CORSMiddleware,
--- app/routers/municipalities.py
+++ app/routers/municipalities.py
@@ -18,9 +18,7 @@ router = APIRouter()
 def read_municipality(
     mun_name: str,
     db: Session = Depends(db_connection),
-    departament: Optional[str | None] = Query(
-        default=None, min_length=6, max_length=12
-    ),
+    departament: Optional[str] = Query(default=None, min_length=6, max_length=12),
 ):
     """Returns towships data in json format."""
 
