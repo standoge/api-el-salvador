82cf25904f622aa7da825a9ab0962ed27565f917
StanDoge <stanlydoge@gmail.com>
2022-12-18 21:06:08 -0600

    `feat:` context scrapper in API for zipcodes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import departaments, municipalities, zones
+from app.routers import departaments, municipalities, zones, scrapper
 
 app = FastAPI()
 
@@ -16,6 +16,7 @@ app.add_middleware(
 app.include_router(departaments.router)
 app.include_router(municipalities.router)
 app.include_router(zones.router)
+app.include_router(scrapper.router)
 
 
 @app.get("/")
--- app/routers/scrapper.py
+++ app/routers/scrapper.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends
-from zipcode.departments import Departments, Endpoint
+from fastapi import APIRouter
+from zipcode.department import Department, Endpoint
 
 router = APIRouter()
 
@@ -7,5 +7,5 @@ router = APIRouter()
 @router.get(path="/scrapper/{dep_name}", tags=["SCRAPPER"])
 def get_zipcodes(dep_name: str):
     """Returns departaments data in json format."""
-    departament = Departments(Endpoint.dep_name.value)
-    return department.zip_codes
+    departament_zipcode = Department(Endpoint[f"{dep_name}"].value)
+    return departament_zipcode.zip_codes
