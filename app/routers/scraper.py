7ecdbf1c2c5582238169f69bb73fb1887eb15248
StanDoge <StanlyDoge@gmail.com>
2022-12-23 15:13:44 -0600

    image scraper function

--- app/routers/scraper.py
+++ app/routers/scraper.py
@@ -1,7 +1,13 @@
+import os
+
 from fastapi import APIRouter
 from zipcode.department import Department, Endpoint
 
+from serpapi import GoogleSearch
+
 router = APIRouter()
+
+G_KEY = os.environ["G_KEY"]
 
 
 @router.get(path="/scraper/{dep_name}", tags=["SCRAPER"])
@@ -9,3 +15,12 @@ def get_zipcodes(dep_name: str):
     """Returns municipalities by department and their zip codes in json format."""
     departament_zipcode = Department(Endpoint[f"{dep_name}"].value)
     return departament_zipcode.zip_codes
+
+
+def dep_imgs(func):
+    params = {"q": f"El Salvador {dep_name}", "tbm": "jin", "ijin": 0, "api_key": G_KEY}
+
+    search = GoogleSearch(params)
+
+    imgs = search.get_dict()["images_results"]
+    return imgs
