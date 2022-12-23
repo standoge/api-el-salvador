883a75af9714fe4c5a94e365a5c4e1f04ae91e31
StanDoge <StanlyDoge@gmail.com>
2022-12-23 15:35:07 -0600

    `feat!:` scraping endpoint for images

--- app/routers/scraper.py
+++ app/routers/scraper.py
@@ -10,17 +10,30 @@ router = APIRouter()
 G_KEY = os.environ["G_KEY"]
 
 
-@router.get(path="/scraper/{dep_name}", tags=["SCRAPER"])
+@router.get(
+    path="/scraper/zipcodes/{dep_name}",
+    tags=["SCRAPER"]
+)
 def get_zipcodes(dep_name: str):
     """Returns municipalities by department and their zip codes in json format."""
     departament_zipcode = Department(Endpoint[f"{dep_name}"].value)
     return departament_zipcode.zip_codes
 
 
-def dep_imgs(func):
-    params = {"q": f"El Salvador {dep_name}", "tbm": "jin", "ijin": 0, "api_key": G_KEY}
+@router.get(
+    path="/scraper/imgs/{dep_name}",
+    tags=["SCRAPER"]
+)
+def get_images(dep_name: str):
+
+    params = {
+        "q": f"El Salvador {dep_name}",
+        "tbm": "isch",
+        "ijin": 0,
+        "api_key": G_KEY,
+    }
 
     search = GoogleSearch(params)
 
-    imgs = search.get_dict()["images_results"]
-    return imgs
+    results = search.get_dict()["images_results"]
+    return results
