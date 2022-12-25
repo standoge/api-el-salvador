63fc88d5fd81c58e8811ee7ec96bc4f48dbdee26
StanDoge <stanlydoge@gmail.com>
2022-12-25 01:02:55 -0600

    `test:` zones endpoint test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 
+import pytest
 from app.main import app
 
 client = TestClient(app)
@@ -16,6 +17,7 @@ def test_department():
     """Test status code & get request for dep endpoint"""
     response = client.get("/departments/San Salvador")
     assert response.status_code == 200
+    # here reponse.json() is a dict, so is needed specify by key
     assert response.json()["depname"] == "San Salvador"
 
 
@@ -27,14 +29,21 @@ def test_municipality():
     assert response.json()[0]["munname"] == "Colón"
 
 
+def test_zone():
+    """Test status code & get request for zone endpoint"""
+    response = client.get("/zones/occidental")
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+
+
 def test_zipcodes():
     """Test status code & get request for zipcodes scraper endpoint"""
     response = client.get("/scraper/zipcodes/la_paz")
     assert response.status_code == 200
-    # here reponse.json() is a dict, so is needed specify by key
     assert response.json()["Zacatecoluca"] == "01601"
 
 
+@pytest.mark.skip(reason="This test consume request attempts")
 def test_images():
     """Test status code & get request for images scraper endpoint"""
     response = client.get("/scraper/images/usulutan")
