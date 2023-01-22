396ff165448ec3298351e74c52a7bf51b08c9567
StanDoge <stanlydoge@gmail.com>
2023-01-21 23:12:33 -0600

    `feat:` minor update

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import departments, municipalities, zones, scraper
+
 from app.internal.middlewares import SyntaxisErrorMiddleware, TimeMiddleware
+from app.routers import departments, municipalities, scraper, zones
 
 app = FastAPI()
 app.title = "Maquilishuat"
-app.version = "1.1.1"
+app.version = "1.2.0"
 
 origins = ["*"]
 
@@ -23,9 +24,3 @@ app.include_router(departments.router)
 app.include_router(municipalities.router)
 app.include_router(zones.router)
 app.include_router(scraper.router)
-
-
-@app.get("/")
-def read_root():
-    """Main endpoint"""
-    return {"Hello": "From FastAPI"}
