6256f0ec9fb5bd36f866defdd4f9ce4dbac11ab4
standoge <stanlydoge@gmail.com>
2024-03-03 10:26:01 -0600

    `fix:` correct path to .db file
    The string interpolation with the variabl wasn't working

--- app/internal/db.py
+++ app/internal/db.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# data = "sqlite:////home/standoge/development/github/api-el-salvador/el_salvador.db"
-data = "sqlite:////home/standoge/development/github/api-el-salvador/el_salvador.db"
-engine = create_engine(data)
+DATABASE = "el_salvador.db"
+engine = create_engine("sqlite:///./el_salvador.db")
 
 # upper 'cause returns a class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
