eb9574ef3d40b8e44d77bd81a589218dcc68e79c
standoge <stanlydoge@gmail.com>
2024-03-03 10:45:35 -0600

    `fix:` DB file path
    The path taken is from project root, not from file that is calling the DB file

--- app/internal/db.py
+++ app/internal/db.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE = "el_salvador.db"
+DATABASE = "./el_salvador.db"
+# this relative path isn't from this file, is from project root
 engine = create_engine(f"sqlite:///{DATABASE}")
 
 # upper 'cause returns a class
